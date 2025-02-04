1fbf4f7c601f39283f4d9bbf1e7eae3fb044f4fd
Etienne <gaumery@hotmail.com>
2025-02-04 12:32:27 +0100

    navbarawww

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Navbar.css";
+
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
-    <nav className="navbar navbar-light bg-light p-3">
-      <div className="container-fluid d-flex justify-content-between align-items-center">
-        <Link className="navbar-brand" to="/">
-          Padel Reservation
+    <nav className="navbar fixed-top navbar-transparent">
+      <div className="container-fluid d-flex justify-content-between align-items-center p-3">
+        <Link className="navbar-brand text-light" to="/">
+          Padel-Mate
         </Link>
-        <button
-          className="btn btn-outline-dark"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          ☰
-        </button>
-      </div>
-      {menuOpen && (
-        <div className="dropdown-menu show position-absolute end-0 mt-2 p-3 shadow bg-white" style={{ right: "20px", top: "50px" }}>
-          <Link className="dropdown-item" to="/profile" onClick={() => setMenuOpen(false)}>Profil</Link>
-          <Link className="dropdown-item" to="/matchmaking" onClick={() => setMenuOpen(false)}>Matchmaking</Link>
-          <Link className="dropdown-item" to="/reservation" onClick={() => setMenuOpen(false)}>Réservation</Link>
-          <Link className="dropdown-item" to="/confirmation" onClick={() => setMenuOpen(false)}>Confirmation</Link>
+        <div className="nav-links">
+          <Link className="nav-item text-light" to="/biciclette">BICICLETTE</Link>
+          <Link className="nav-item text-light" to="/sarto-world">SARTO WORLD</Link>
+          <Link className="nav-item text-light" to="/journal">JOURNAL</Link>
+          <Link className="nav-item text-light" to="/supporto">SUPPORTO</Link>
         </div>
-      )}
+      </div>
     </nav>
   );
 };
