393a4896bb6306fb8cc70c1df60baaa22c4644fb
Etienne <gaumery@hotmail.com>
2025-02-04 18:18:01 +0100

    background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,18 @@ import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
 
-
 const Navbar = () => {
   return (
     <nav className="navbar fixed-top navbar-transparent">
       <div className="container-fluid d-flex justify-content-between align-items-center p-3">
-        <Link className="navbar-brand text-light" to="/">
+        <Link className="navbar-brand text-light" to="/home">
           Padel-Mate
         </Link>
         <div className="nav-links">
-          <Link className="nav-item text-light" to="/biciclette">BICICLETTE</Link>
-          <Link className="nav-item text-light" to="/sarto-world">SARTO WORLD</Link>
-          <Link className="nav-item text-light" to="/journal">JOURNAL</Link>
-          <Link className="nav-item text-light" to="/supporto">SUPPORTO</Link>
+          <Link className="nav-item text-light" to="/matchmaking">Matchmaking</Link>
+          <Link className="nav-item text-light" to="/profile">###</Link>
+          <Link className="nav-item text-light" to="/reservation">Reservation</Link>
+          <Link className="nav-item text-light" to="/profile">Profile</Link>
         </div>
       </div>
     </nav>
