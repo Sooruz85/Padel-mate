d402c9b0b7ecfbfe8ea63ef9e7280f517055460f
Etienne <gaumery@hotmail.com>
2025-02-05 18:34:08 +0100

    home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,13 @@ const Navbar = () => {
   return (
     <nav className="navbar fixed-top navbar-transparent">
       <div className="container-fluid d-flex justify-content-between align-items-center p-3">
-        <Link className="navbar-brand text-light" to="/home">
+        <Link className="navbar-brand text-light" to="/">
           Padel-Mate
         </Link>
         <div className="nav-links">
           <Link className="nav-item text-light" to="/matchmaking">Matchmaking</Link>
-          <Link className="nav-item text-light" to="/profile">###</Link>
-          <Link className="nav-item text-light" to="/reservation">Reservation</Link>
           <Link className="nav-item text-light" to="/profile">Profile</Link>
+          <Link className="nav-item text-light" to="/reservation">Reservation</Link>
         </div>
       </div>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 
 const Home = () => {
   return (
     <div className="home-container">
-      <header className="home-header">
-      </header>
+      <header className="home-header"></header>
       <main className="home-content">
         <h1>Bienvenue sur Padel Reservation</h1>
         <p>Réservez facilement un terrain de padel et trouvez des partenaires de jeu.</p>
         <div className="auth-forms">
+          {/* Formulaire de connexion */}
           <form className="sign-in">
             <h2>Se connecter</h2>
             <input type="email" placeholder="Email" required />
             <input type="password" placeholder="Mot de passe" required />
             <button type="submit">Connexion</button>
           </form>
+
+          {/* Formulaire d'inscription */}
           <form className="sign-up">
             <h2>S'inscrire</h2>
             <input type="text" placeholder="Nom" required />
